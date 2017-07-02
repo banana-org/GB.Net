1602cfdf8273e0fd44c10c6ab1fd010202afc873
Saghen <megamawman@gmail.com>
2017-07-02 11:47:18 -0400

    Idea and Map support

--- GB.Net Project/GamebananaApi/DataTypes/Idea.cs
+++ GB.Net Project/GamebananaApi/DataTypes/Idea.cs
@@ -8,5 +8,32 @@ namespace GamebananaApi.DataTypes
 {
     class Idea
     {
+        public readonly int itemID;
+        public readonly string fields = "contestid,date,description,feedback_instructions,Flags().aGetActiveFlagRowIds(),Flags().nGetActiveFlagsCount(),lastpost_date,lastpost_userid,mdate,modnote,name,postcount,screenshots,Stats().nGetNewestSubmissionId(),Stats().nGetSubmissionCount(),text,Trash().bIsTrashed(),userid,views,Withhold().bIsWithheld()";
+        public Idea(int ID)
+        {
+            itemID = ID;
+        }
+
+        public int contestid { get; set; }
+        public int date { get; set; }
+        public string description { get; set; }
+        public string feedback_instructions { get; set; }
+        public int[] FlagsaGetActiveFlagRowIds { get; set; }
+        public int FlagsnGetActiveFlagsCount { get; set; }
+        public int lastpost_date { get; set; }
+        public int lastpost_userid { get; set; }
+        public int mdate { get; set; }
+        public string modnote { get; set; }
+        public string name { get; set; }
+        public int postcount { get; set; }
+        public string screenshots { get; set; }
+        public int StatsnGetNewestSubmissionId { get; set; }
+        public int StatsnGetSubmissionCount { get; set; }
+        public string text { get; set; }
+        public bool TrashbIsTrashed { get; set; }
+        public int userid { get; set; }
+        public int views { get; set; }
+        public bool WithholdbIsWithheld { get; set; }
     }
 }
--- GB.Net Project/GamebananaApi/DataTypes/Map.cs
+++ GB.Net Project/GamebananaApi/DataTypes/Map.cs
@@ -8,5 +8,51 @@ namespace GamebananaApi.DataTypes
 {
     class Map
     {
+        public readonly int itemID;
+        public readonly string fields = "apps_used,authors,catid,contestid,creator,cumulative_rating,date,description,dev_state,Downloadable().nGetFilesize(),Downloadable().sFileUrl(),downloads,feedback_instructions,file,Flags().aGetActiveFlagRowIds(),Flags().nGetActiveFlagsCount(),lastpost_date,lastpost_userid,mdate,modnote,name,players,postcount,projectid,rating,Rating().SkillLevel().sGetGroupName(),Rating().SkillLevel().sGetTitle(),screenshots,Stats().nGetNewestSubmissionId(),Stats().nGetSubmissionCount(),studioid,tags,text,Trash().bIsTrashed(),userid,version,views,votes,Withhold().bIsWithheld()";
+        public Map(int ID)
+        {
+            itemID = ID;
+        }
+
+        public string apps_used { get; set; }
+        public string authors { get; set; }
+        public int catid { get; set; }
+        public int contestid { get; set; }
+        public string creator { get; set; }
+        public string cumulative_rating { get; set; }
+        public int date { get; set; }
+        public string description { get; set; }
+        public string dev_state { get; set; }
+        public int DownloadablenGetFilesize { get; set; }
+        public string DownloadablesFileUrl { get; set; }
+        public int downloads { get; set; }
+        public string feedback_instructions { get; set; }   
+        public string file { get; set; }
+        public int[] FlagsaGetActiveFlagRowIds { get; set; }
+        public int FlagsnGetActiveFlagsCount { get; set; }
+        public int lastpost_date { get; set; }
+        public int lastpost_userid { get; set; }
+        public int mdate { get; set; }
+        public string modnote { get; set; }
+        public string name { get; set; }
+        public string players { get; set; }
+        public int postcount { get; set; }
+        public int projectid { get; set; }
+        public string rating { get; set; }
+        public string RatingSkillLevelsGetGroupName { get; set; }
+        public string RatingSkillLevelsGetTitle { get; set; }
+        public string screenshots { get; set; }
+        public int StatsnGetNewestSubmissionId { get; set; }
+        public int StatsnGetSubmissionCount { get; set; }
+        public int studioid { get; set; }
+        public string tags { get; set; }
+        public string text { get; set; }
+        public bool TrashbIsTrashed { get; set; }
+        public int userid { get; set; }
+        public string version { get; set; }
+        public int views { get; set; }
+        public int votes { get; set; }
+        public bool WithholdbIsWithheld { get; set; }
     }
 }
